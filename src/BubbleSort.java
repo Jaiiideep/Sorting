470a3c3069bd0586291413d6c91932b0974328cd
jaide <jaide@DESKTOP-8CP0L2F>
2021-02-10 00:23:36 -0400

    Merge Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,17 +1,6 @@
 
 public class BubbleSort {
-	public static void main(String[] args) {
-		int[] arr = {5, 4, 3, 2, 1};
-		System.out.print("Array before sorting: ");
-		printArray(arr);
-		
-		arr = bubbleSort(arr);
-		
-		System.out.print("Array after sorting: ");
-		printArray(arr);
-	}
-
-	private static int[] bubbleSort(int[] arr) {
+	void bubbleSort(int arr[]) {
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++)
 		{
@@ -22,14 +11,25 @@ public class BubbleSort {
 				arr[j + 1] = temp;
 			}
 		}
-		return arr;
 	}
 	
-	private static void printArray(int arr[]) {
+	static void printArray(int arr[]) {
 		int n = arr.length;
 		for (int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {5, 4, 3, 2, 1};
+		System.out.print("Array before sorting: ");
+		printArray(arr);
+		
+		BubbleSort ob = new BubbleSort();
+		ob.bubbleSort(arr);
+		
+		System.out.print("Array after sorting: ");
+		printArray(arr);
 	}
 }
