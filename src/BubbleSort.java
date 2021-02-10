1aad4a67b2dba5960530711c0fa74949c4df907a
Jaideep Singh Chahal <E19CSE449@bennett.edu.in>
2021-02-10 00:34:43 -0400

    Code breaking commit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -22,7 +22,7 @@ public class BubbleSort {
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = {5, 4, 3, 2, 1};
+		int[] ar = {5, 4, 3, 2, 1};
 		System.out.print("Array before sorting: ");
 		printArray(arr);
 		
